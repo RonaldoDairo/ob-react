36808edaa06ca32bfa74b0e14943fbc651a49707
Martín San José de Vicente <martin@Martins-MacBook.local>
2021-10-06 11:12:47 +0200

    Ejemplos de hooks realizados

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Greeting from './components/pure/greeting';
 import Greetingf from './components/pure/greetingF';
 import TaskListComponent from './components/container/task_list';
+import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import MiComponenteConContexto from './hooks/Ejemplo3'
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -14,7 +18,17 @@ function App() {
         {/* Componente de ejemplo funcional */}
         {/* <Greetingf name="Martín"></Greetingf> */}
         {/* Componente de Listado de Tareas */}
-        <TaskListComponent></TaskListComponent>
+        {/* <TaskListComponent></TaskListComponent> */}
+        {/* Ejemplos de uso de HOOKS */}
+        {/* <Ejemplo1></Ejemplo1> */}
+        {/* <Ejemplo2></Ejemplo2> */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        <Ejemplo4 nombre="Martín">
+          {/* Todo loq ue hay aquí, es tratado como props.children */}
+          <h3>
+            Contenido del props.children
+          </h3>
+        </Ejemplo4>
       </header>
     </div>
   );
