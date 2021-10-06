b920cd851c81a27dd2a76a00d8adc6db0c8cd003
Martín San José de Vicente <martin@Martins-MacBook.local>
2021-10-06 12:00:25 +0200

    Ejemplos de uso de hooks y ciclos de vida

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LEVELS } from '../../models/levels.enum';
 import { Task } from '../../models/task.class'
 import TaskComponent from '../pure/task';
@@ -7,7 +7,19 @@ const TaskListComponent = () => {
 
     const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
 
-    const changeState = (id) => {
+    // Estado del componente
+    const [tasks, setTasks] = useState([defaultTask]);
+
+    // Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log('Task State has been modified');
+        return () => {
+            console.log('TaskList component is going to unmount...')
+        }
+    }, [tasks])
+
+
+    const changeCompleted = (id) => {
         console.log('TODO: Cambiar estado de una tarea')
     }
 
